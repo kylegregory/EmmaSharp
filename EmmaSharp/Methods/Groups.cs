91b62b80e3d1a3a6dfa1ce9d3e60e29cc1f7b54c
Kyle Gregory <kyle.gregory@lipscomb.edu>
2015-03-19 15:06:14 -0500

    Check-in Groups.cs file

---
+++ EmmaSharp/Methods/Groups.cs
@@ -0,0 +1,207 @@
+using EmmaSharp.Models;
+using EmmaSharp.Models.Groups;
+using EmmaSharp.Models.Members;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace EmmaSharp
+{
+    public partial class EmmaApi
+    {
+        #region Groups
+
+        /// <summary>
+        /// Get a basic listing of all active member groups for a single account.
+        /// </summary>
+        /// <param name="groupType">Accepts a comma-separated string with one or more GroupTypes. Defaults to Group.</param>
+        /// <returns>An array of groups.</returns>
+        public AllGroups ListGroups(GroupType groupType)
+        {
+            var request = new RestRequest();
+            request.Resource = "/{accountId}/groups";
+
+            if (groupType != null) 
+                request.AddParameter("group_types", groupType);
+
+            return Execute<AllGroups>(request);
+        }
+
+        /// <summary>
+        /// Create one or more new member groups.
+        /// </summary>
+        /// <param name="groups">A Group to be created. Each object must contain a group_name parameter.</param>
+        /// <returns>An array of the new group ids and group names.</returns>
+        public AllGroups CreateFields(AllGroups groups)
+        {
+            var request = new RestRequest(Method.POST);
+            request.Resource = "/{accountId}/groups";
+            request.AddBody(groups);
+
+            return Execute<AllGroups>(request);
+        }
+
+        /// <summary>
+        /// Get the detailed information for a single member group.
+        /// </summary>
+        /// <param name="memberGroupId">The Member Group Id to be retrieved.</param>
+        /// <returns>A group.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public Group GetGroup(string memberGroupId)
+        {
+            var request = new RestRequest();
+            request.Resource = "/{accountId}/groups/{memberGroupId}";
+            request.AddUrlSegment("memberIdGroup", memberGroupId);
+
+            return Execute<Group>(request);
+        }
+
+        /// <summary>
+        /// Update information for a single member group.
+        /// </summary>
+        /// <param name="group">The Group to be updated.</param>
+        /// <param name="memberGroupId">The Member Group Id to be updated.</param>
+        /// <returns>True if the update was successful</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public Group UpdateGroup(Group group, string memberGroupId)
+        {
+            var request = new RestRequest(Method.PUT);
+            request.Resource = "/{accountId}/groups/{memberIdGroup}";
+            request.AddUrlSegment("memberIdGroup", memberGroupId);
+            request.AddBody(group.ActiveCount);
+
+            return Execute<Group>(request);
+        }
+
+        /// <summary>
+        /// Delete a single member group.
+        /// </summary>
+        /// <param name="memberIdGroup">The Member Group Id to be deleted.</param>
+        /// <returns>True if the group is deleted.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public bool DeleteGroup(string memberIdGroup)
+        {
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/{accountId}/groups/{memberIdGroup}";
+            request.AddUrlSegment("memberIdGroup", memberIdGroup);
+
+            return Execute<bool>(request);
+        }
+        #endregion
+
+        #region GroupMembers
+
+        /// <summary>
+        /// Get the members in a single active member group.
+        /// </summary>
+        /// <param name="memberGroupId">The Member Group Id to be retrieved.</param>
+        /// <param name="deleted">Include deleted members. Optional, defaults to false.</param>
+        /// <returns>An array of members.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public GroupMembers ListGroupMembers(string memberGroupId, bool deleted = false)
+        {
+            var request = new RestRequest();
+            request.Resource = "/{accountId}/groups/{memberGroupId}/members";
+            request.AddUrlSegment("memberIdGroup", memberGroupId);
+
+            if (deleted != false)
+                request.AddParameter("deleted", deleted);
+
+            return Execute<GroupMembers>(request);
+        }
+
+        /// <summary>
+        /// Add a list of members to a single active member group.
+        /// </summary>
+        /// <param name="memberGroupId">The Member Group Id to be retrieved.</param>
+        /// <param name="memberIds">An array of member ids.</param>
+        /// <returns>An array of references to the members added to the group. If a member already exists in the group or is not a valid member, that reference will not be returned.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public List<string> AddMembersToGroup(string memberGroupId, List<string> memberIds)
+        {
+            var request = new RestRequest(Method.PUT);
+            request.Resource = "/{accountId}/groups/{memberGroupId}/members";
+            request.AddUrlSegment("memberIdGroup", memberGroupId);
+            request.AddBody(memberIds);
+
+            return Execute<List<string>>(request);
+        }
+
+        /// <summary>
+        /// Remove members from a single active member group.
+        /// </summary>
+        /// <param name="memberGroupId">The Member Group Id to be retrieved.</param>
+        /// <param name="memberIds">An array of member ids.</param>
+        /// <returns>An array of references to the removed members.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public List<string> RemoveMembersFromGroup(string memberGroupId, List<string> memberIds) 
+        {
+            var request = new RestRequest(Method.PUT);
+            request.Resource = "/{accountId}/groups/{memberGroupId}/members/remove";
+            request.AddUrlSegment("memberIdGroup", memberGroupId);
+            request.AddBody(memberIds);
+
+            return Execute<List<string>>(request);
+        }
+
+        /// <summary>
+        /// Remove all members from a single active member group.
+        /// </summary>
+        /// <param name="memberGroupId">The Member Group Id to be retrieved.</param>
+        /// <param name="status">A Member Status string. Optional. This is ‘a’ (active), ‘o’ (optout), or ‘e’ (error).</param>
+        /// <returns>Returns the number of members removed from the group.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public int DeleteAllMembersFromGroup(string memberGroupId, MemberStatus status = null)
+        {
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/{accountId}/groups/{memberGroupId}/members";
+            request.AddUrlSegment("memberIdGroup", memberGroupId);
+
+            if (status != null)
+                request.AddParameter("member_status_id", status);
+
+            return Execute<int>(request);
+        }
+
+        /// <summary>
+        /// Remove all members from all active member groups as a background job. The member_status_id parameter must be set.
+        /// </summary>
+        /// <param name="memberGroupId">The Member Group Id to be retrieved.</param>
+        /// <param name="status">A Member Status string. Optional. This is ‘a’ (active), ‘o’ (optout), or ‘e’ (error).</param>
+        /// <returns>Returns true.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public bool DeleteAllMembersFromAllGroups(string memberGroupId, MemberStatus status = null)
+        {
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/{accountId}/groups/{memberGroupId}/members/remove";
+            request.AddUrlSegment("memberIdGroup", memberGroupId);
+
+            if (status != null)
+                request.AddParameter("member_status_id", status);
+
+            return Execute<bool>(request);
+        }
+
+        /// <summary>
+        /// Copy all the users of one group into another group
+        /// </summary>
+        /// <param name="fromGroupId">The Member Group ID to be copied from.</param>
+        /// <param name="toGroupId">The Member Group ID to be copied to.</param>
+        /// <param name="status">A Member Status string. Optional. This is ‘a’ (active), ‘o’ (optout), or ‘e’ (error).</param>
+        /// <returns>Returns true.</returns>
+        /// <exception cref="HttpResponseException">Http404 if the group does not exist.</exception>
+        public bool CopyUsersFromGroup(string fromGroupId, string toGroupId, MemberStatus status = null)
+        {
+            var request = new RestRequest(Method.DELETE);
+            request.Resource = "/{accountId}/groups/{fromGroupId}/{toGroupId}/members/copy";
+            request.AddUrlSegment("memberIdGroup", fromGroupId);
+            request.AddUrlSegment("memberIdGroup", toGroupId);
+
+            if (status != null)
+                request.AddParameter("member_status_id", status);
+
+            return Execute<bool>(request);
+        }
+        #endregion
+
+    }
+}
