2adc5d54f1e920a8115bfa263cc7c225b5a73dd4
Kyle Gregory <kyle.gregory@lipscomb.edu>
2015-06-01 13:13:23 -0500

    Added SignupForms Methods.

---
+++ EmmaSharp/Methods/SignupForms.cs
@@ -0,0 +1,36 @@
+﻿using EmmaSharp.Models.SignupForms;
+using RestSharp;
+using System.Collections.Generic;
+
+namespace EmmaSharp.Methods
+{
+    public class SignupForms : EmmaApi
+    {
+        #region SignupForms
+
+        /// <summary>
+        /// Signup Forms Endpoints
+        /// </summary>
+        /// <param name="publicKey">The account's public key.</param>
+        /// <param name="secretKey">The account's private key.</param>
+        /// <param name="accountId">The account id.</param>
+        public SignupForms(string publicKey, string secretKey, string accountId)
+            : base(publicKey, secretKey, accountId)
+        {
+        }
+
+        /// <summary>
+        /// Gets a list of this account’s signup forms.
+        /// </summary>
+        /// <returns>An array of signup forms.</returns>
+        public List<SignupForm> GetSignupForms()
+        {
+            var request = new RestRequest();
+            request.Resource = "/{accountId}/signup_forms";
+
+            return Execute<List<SignupForm>>(request);
+        }
+
+        #endregion
+    }
+}
---
+++ EmmaSharp/Models/SignupForms/SignupForm.cs
@@ -0,0 +1,18 @@
+﻿using RestSharp.Deserializers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaSharp.Models.SignupForms
+{
+    public class SignupForm
+    {
+        [DeserializeAs(Name = "id")]
+        public int SignupFormId { get; set; }
+
+        [DeserializeAs(Name = "name")]
+        public string Name { get; set; }
+    }
+}
