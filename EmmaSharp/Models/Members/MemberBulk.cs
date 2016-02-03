a8d874d393b1653368130a54a8d129a60c8b265f
MikeSmithDev <mike@mikesmithdev.com>
2016-02-03 16:38:17 -0500

    Updated AddNewMembers and related classes

---
+++ EmmaSharp/Models/Members/MemberBulk.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace EmmaSharp.Models
+{
+    /// <summary>
+    /// Used to add new members or update existing members in bulk.
+    /// </summary>
+    public class MemberBulk
+    {
+        /// <summary>
+        /// Email address of member to add or update
+        /// </summary>
+        [JsonProperty("email")]
+        public string MemberEmail { get; set; }
+
+        /// <summary>
+        /// Names and values of user-defined fields to update
+        /// </summary>
+        [JsonProperty("fields")]
+        public Dictionary<string, string> Fields { get; set; }
+    }
+}
---
+++ EmmaSharp/Models/Members/MembersAdd.cs
@@ -0,0 +1,12 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace EmmaSharp.Models.Members
+{
+    public class MembersAdd
+    {
+        [JsonProperty("import_id")]
+        public int ImportId { get; set; }
+    }
+}
