0d11df96972a8574ee49324d74f41d713bd1bf07
Kyle Gregory <kyle.gregory@lipscomb.edu>
2016-03-23 16:32:14 -0500

    Closes #18

--- EmmaSharp/Models/Members/ChangeStatus.cs
+++ EmmaSharp/Models/Members/ChangeStatus.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 ﻿using Newtonsoft.Json;
 using System;
 using System.Collections.Generic;
@@ -25,3 +26,32 @@ namespace EmmaSharp.Models.Members
         public MemberStatusShort StatusTo { get; set; }
     }
 }
+=======
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaSharp.Models.Members
+{
+    /// <summary>
+    /// Change the status for an array of members.
+    /// </summary>
+    public class ChangeStatus
+    {
+        /// <summary>
+        /// The array of member ids to change.
+        /// </summary>
+        [JsonProperty("member_ids")]
+        public List<int> MemberIds { get; set; }
+
+        /// <summary>
+        /// The new status for the given members. Accepts one of ‘a’ (active), ‘e’ (error), ‘o’ (optout).
+        /// </summary>
+        [JsonProperty("status_to")]
+        public MemberStatusShort StatusTo { get; set; }
+    }
+}
+>>>>>>> edcb3aaaa0106b13ae727a75e0f58b0af394b21d
---
+++ EmmaSharp/Models/Webhooks/WebhookPostMemberSignup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using EmmaSharp.Extensions;
+using Newtonsoft.Json;
+
+namespace EmmaSharp.Models.Webhooks
+{
+    public class WebhookPostMemberSignup
+    {
+        [JsonProperty("event_name")]
+        public string EventName { get; set; }
+
+        [JsonProperty("resource_url", NullValueHandling = NullValueHandling.Ignore)]
+        public string ResourceUrl { get; set; }
+
+        [JsonProperty("data")]
+        public WebhookPostDataMemberSignup Data { get; set; }
+    }
+
+    public class WebhookPostDataMemberSignup
+    {
+        [JsonProperty("signup_form_id", NullValueHandling = NullValueHandling.Ignore)]
+        public string SignupFormId { get; set; }
+
+        [JsonProperty("account_id")]
+        public string AccountId { get; set; }
+
+        [JsonProperty("member_id")]
+        public string MemberId { get; set; }
+
+        [JsonProperty("mailing_id", NullValueHandling = NullValueHandling.Ignore)]
+        public string MailingId { get; set; }
+
+        [JsonConverter(typeof(EmmaDateConverter))]
+        [JsonProperty("timestamp")]
+        public DateTime Timestamp { get; set; }
+
+    }
+}
