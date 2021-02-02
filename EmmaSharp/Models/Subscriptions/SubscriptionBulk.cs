496b3797605c7f374ffbac2c3f435c563f26894d
Joseph Namey <joseph3114@gmail.com>
2021-02-02 11:00:34 -0500

    Added subscription objects

---
+++ EmmaSharp/Models/Subscriptions/SubscriptionBulk.cs
@@ -0,0 +1,15 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaSharp.Models.Subscriptions
+{
+    public class SubscriptionBulk
+    {
+        [JsonProperty("member_ids")]
+        public List<int> MemberIds { get; set; }
+    }
+}
---
+++ EmmaSharp/Models/Subscriptions/SubscriptionMembers.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmmaSharp.Models.Subscriptions
+{
+    public class SubscriptionMembers
+    {
+        public List<Individual> MemberIds { get; set; }
+    }
+    public class Individual
+    {
+        [JsonProperty("member_id")]
+        public int MemberId { get; set; }
+    }
+}
